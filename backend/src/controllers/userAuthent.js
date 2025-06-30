0041ad93848858ed63eee9c5e94b59c48ed3bfe5
yadav04anurag <yadavanurag1403@gmail.com>
2025-06-30 09:50:49 +0530

    some deploy changes

--- backend/src/controllers/userAuthent.js
+++ backend/src/controllers/userAuthent.js
@@ -27,7 +27,7 @@ const register = async (req,res)=>{
         role:user.role,
     }
     
-     res.cookie('token',token,{maxAge: 60*60*1000});
+     res.cookie('token',token,{path:'/',httpOnly:'false',maxAge: 60*60*1000});
      res.status(201).json({
         user:reply,
         message:"Loggin Successfully"
@@ -64,7 +64,7 @@ const login = async (req,res)=>{
         }
 
         const token =  jwt.sign({_id:user._id , emailId:emailId, role:user.role},process.env.JWT_KEY,{expiresIn: 60*60});
-        res.cookie('token',token,{maxAge: 60*60*1000});
+        res.cookie('token',token,{path:'/',httpOnly:'false',maxAge: 60*60*1000});
         res.status(201).json({
             user:reply,
             message:"Loggin Successfully"
