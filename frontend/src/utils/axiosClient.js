fafa030934da98655497deeed5490786431be03c
yadav04anurag <164538440+yadav04anurag@users.noreply.github.com>
2025-07-18 10:52:58 +0530

    Update axiosClient.js

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const axiosClient =  axios.create({
    // baseURL: 'http://localhost:3000',
     
-   baseURL: 'https://code-shastra-awq7.vercel.app',
+   baseURL: 'https://code-shastra-a7zx.vercel.app',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
