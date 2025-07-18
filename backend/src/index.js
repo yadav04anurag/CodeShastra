2d98a04309c92b3045015ecb03ec2d7524c3b092
yadav04anurag <164538440+yadav04anurag@users.noreply.github.com>
2025-07-18 10:54:31 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,6 +19,7 @@ app.use(cors({
     //origin: 'http://localhost:5173',
     origin:['http://localhost:5173','https://code-shastra.vercel.app','http://localhost:5173','https://code-shastra-*.vercel.app'],
     credentials: true,
+     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
